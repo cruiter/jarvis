9c345f2571a7c3fd131b1e5e2cf47eb93475525d
Ryan Baker <rbaker11@asu.edu>
2017-01-30 22:31:52 -0800

    60 Add checkEC2 function

--- src/AWS_API.js
+++ src/AWS_API.js
@@ -12,11 +12,46 @@ exports.checkEC2Instance = function(instanceId, callback) {
     }
     EC2.describeInstances(params, function(err, data) {
         if (err) {
-            console.log(err, err.stack);
-            return;
+            return console.err(err, err.stack);
         }
 
         //console.log(JSON.stringify(data, null, 2));
         callback(data.Reservations[0].Instances[0].State.Name === 'running');
+    });
+}
+
+exports.checkEC2 = function(callback) {
+    //query for the status of all instances
+    EC2.describeInstances({}, function(err, data) {
+        if (err) {
+            return console.err(err, err.stack);
+        }
+
+        var instances = data.Reservations[0].Instances;
+        var nonRunningInstances = [];
+
+        //look for all of the instances that are not running
+        for (var i = instances.length - 1; i >= 0; i--) {
+            if (instances[i].State.Name !== 'running') {
+                nonRunningInstances.push(instances[i].InstanceId);
+            }
+        }
+
+        // nonRunningInstances.push('i-6634291699');
+        // nonRunningInstances.push('i-128397357');
+        // nonRunningInstances.push('i-2725972359');
+
+        if (nonRunningInstances.length < 1) {
+            callback('Everything is running. All good!');
+        } else {
+            var message = 'The instances: ';
+            for (var i = nonRunningInstances.length - 1; i >= 0; i--) {
+                message += nonRunningInstances[i] + ', ';
+            }
+            message = message.substring(0, message.length - 2);
+            message += ' are not running right now. Check them out!';
+
+            callback(message);
+        }
     });
 }

6f1a46ef2c5be720debeaee3686260b985f76f2f
Christopher Ruiter <christopnerruiter@gmail.com>
2017-02-03 18:19:35 -0700

    Minor changes

--- src/SlackInterface.js
+++ src/SlackInterface.js
@@ -4,21 +4,34 @@
 
 /* eslint no-console:0 */
 
+
+/* SLACK API*/
 var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-var mainController = require('./mainController');
 var MemoryDataStore = require('@slack/client').MemoryDataStore;
 
+/*Jarvis Include*/
+var mainController = require('./mainController');
 
 
+//Grabs token form the environment. Initialized with startBot.sh
 var token = process.env.SLACK_API_TOKEN || '';
 
+//REAL TIME MESSAGING API
 var rtm = new RtmClient(token);
+
+//Start Jarvis Server
 rtm.start();
+
+//Online Message
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function handleRTMAuthenticated() {
+  console.log('Jarvis is Online.');
+});
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   console.log('Message:', message);
-
+ // mainControllermessage.text;
   //rtm.sendMessage(mainController.git();
 	rtm.sendMessage("User "+rtm.dataStore.getUserById(message.user).name+' posted a message in '+rtm.dataStore.getChannelGroupOrDMById(message.channel).name+" channel", message.channel);
     
@@ -33,3 +46,4 @@ rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
 rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
   console.log('Reaction removed:', reaction);
 });
+
